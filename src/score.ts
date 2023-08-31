b472f26a24d32e23b1f639ce6dce5f48e466eb48
printf83 <printf83@gmail.com>
2023-08-31 13:12:00 +0800

    update

--- src/score.ts
+++ src/score.ts
@@ -9,27 +9,29 @@ export class score {
 	speed_x: number;
 	speed_y: number;
 	mark_delete: boolean;
-
+	timer: number;
 	constructor(opt: { text: string; x: number; y: number; destination_x: number; destination_y: number }) {
 		this.text = opt.text;
 		this.x = opt.x;
 		this.y = opt.y;
 		this.destination_x = opt.destination_x;
 		this.destination_y = opt.destination_y;
-		this.speed_x = 1;
-		this.speed_y = 1;
-
-		// this.speed_x = Math.sqrt(this.x + this.y);
-		// this.speed_y = Math.sqrt(this.destination_x + this.destination_y);
+		this.speed_x = (this.destination_x - this.x) * 0.01;
+		this.speed_y = (this.destination_y - this.y) * 0.01;
 		this.mark_delete = false;
+		this.timer = 1000;
 	}
 	update() {
-		if (this.x > this.destination_x) this.x -= this.speed_x;
-		if (this.x < this.destination_x) this.x += this.speed_x;
-		if (this.y > this.destination_y) this.y -= this.speed_y;
-		if (this.y < this.destination_y) this.y += this.speed_y;
+		// if (this.x > this.destination_x) this.x -= this.speed_x;
+		// if (this.x < this.destination_x) this.x += this.speed_x;
+		// if (this.y > this.destination_y) this.y -= this.speed_y;
+		// if (this.y < this.destination_y) this.y += this.speed_y;
 
-		if (this.x <= this.destination_x + 5 && this.y <= this.destination_y + 5) this.mark_delete = true;
+		this.x += this.speed_x;
+		this.y += this.speed_y;
+
+		this.timer--;
+		if (this.timer <= 0) this.mark_delete = true;
 	}
 	draw(opt: { ctx: CanvasRenderingContext2D }) {
 		draw_text({
