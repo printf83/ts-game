e15a59cad2fd5a5a4c2283c30be1a557bdc3bb71
printf83 <printf83@gmail.com>
2023-09-03 01:31:49 +0800

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Base on JavaScript Game Development Course for Beginners https://www.youtube.com/watch?v=GFO_txvwK_c&t=13054s
-
-Asset by https://bevouliin.com
--- scripts/copy.cjs
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-
-console.log("Copy static file...");
-fs.cpSync("./static", "./dist", { recursive: true });
-console.log("Successfully copy static file!");
