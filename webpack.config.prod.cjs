0b200c5825853d37f4cc0c23ea090fa10fc0ab20
printf83 <printf83@gmail.com>
2023-09-03 23:58:26 +0800

    update

---
+++ webpack.config.prod.cjs
@@ -0,0 +1,37 @@
+const path = require("path");
+const CompressionPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+module.exports = {
+	mode: "production",
+	entry: "./src/index.ts",
+	plugins: [new CompressionPlugin()],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					mangle: true,
+				},
+			}),
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+			},
+		],
+	},
+	resolve: {
+		extensionAlias: {
+			".js": [".ts", ".js"],
+		},
+		modules: ["node_modules"],
+		extensions: [".ts", ".js", ".json"],
+	},
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "./dist/src"),
+	},
+};
