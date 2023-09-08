a4b06f92a758916f9970cc46b124cc551f85d017
Hamzah Aziezs <printf83@gmail.com>
2023-09-08 17:08:53 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 //base on : https://www.youtube.com/watch?v=GFO_txvwK_c&t=13054s
 
 import { game } from "./game.js";
-const DEBUG = true;
+const DEBUG = false;
 const canvas_game = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const canvas_static = document.getElementById("guiCanvas") as HTMLCanvasElement;
 const canvas_value = document.getElementById("valueCanvas") as HTMLCanvasElement;
